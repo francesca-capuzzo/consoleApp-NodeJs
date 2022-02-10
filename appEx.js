360fca0dd4b61a473b2acabc93b4fbe52fe6c0db
Francesca Capuzzo <f.capuzzo9966@gmail.com>
2022-02-10 19:09:18 +0100

    update not finished

--- appEx.js
+++ appEx.js
@@ -1,47 +1,34 @@
 "use strict"
 
+// const { log } = require('console');
+const fs = require('fs');
 
-const fs = require('fs')
+const { Parser, PartialInvalidStringError, InvalidStringError, EmptyStringError } = require('./parserEx.js');
 
-const {Parser, PartialInvalidError, InvalidStringError, EmptyStringError} = require("./parser.js") //--> l'estensione non è obbligatoria ma il./ si!!
 
-const arg = process.argv.slice(2);     
+const arg = process.argv.slice(2);
+
 const fileToRead = arg[0];
+
 const fileToWrite = arg[1];
 
-let result;
-let data;
-try {
-    
-    data = fs.readFileSync(fileToRead, "utf8")
-    result = Parser.parseCsvLine(data);
-    let result2 = Parser.sumArray(result);
-    console.log(result2)
-} catch (error) {
-    console.error(error.message);
-    
-    if(error instanceof PartialInvalidStringError){
-        result = error.partialResult;
-        let result2 = Parser.sumArray(result);
-        console.log(result2);
-    }
-}
 
-let array = [];
+let array;
 
 try {
-    array = Parser.parseCsvLine(data);
-    console.log("array", array);
-    console.log("sum", array.reduce((a,b)=>a+b));
-} catch (error) {
-    console.log(error.message);
-    if (error instanceof PartialInvalidError) {
-        array = error.partialResult;
-    }
+    const data = fs.readFileSync(fileToRead, 'utf8');
+    array = Parser.csvParse(data);
+    // console.log(data);
+    // console.log(array);
+
+} catch (err) {
+    console.error(err);
 }
 
-try{
-    fs.writeFileSync(fileToWrite, JSON.stringify(array));      
-} catch (error) {                                                  
-    console.log(error.message);
+
+
+try {
+    fs.writeFileSync(fileToWrite, JSON.stringify(array));
+} catch (error) {
+    console.log(error);
 }
--- parserEx.js
+++ parserEx.js
@@ -1,11 +1,34 @@
+class EmptyStringError extends Error {
+    constructor(message) {
+        super(message);
+    }
+}
+
+class InvalidStringError extends Error {
+    constructor(message) {
+        super(message);
+    }
+}
+
+
+class PartialInvalidStringError extends Error {
+    constructor(message, array) {
+        super(message)
+        this.array = array;
+
+    }
+}
+
 class Parser {
 
     static csvParse(string) {
-        if (string.length === 0  ) {
+        if (string.length === 0) {
             throw new EmptyStringError("Stringa vuota")
         }
         const tempString = Parser.ReplaceAll(string);
+        console.log("temp string",tempString);
         const tempArray = Parser.Splitter(tempString);
+        console.log("temp array",tempArray);
         let newArray = []
         for (let i = 0; i < tempArray.length; i++) {
             let intArray = Parser.checkParse(tempArray[i])
@@ -13,18 +36,19 @@ class Parser {
 
         }
         //const newArray = Parser.checkParse(tempArray);
+        console.log("new array",newArray);
         return newArray;
     }
-  
-    static ReplaceAll(string){
+
+    static ReplaceAll(string) {
         let stringNumber = string;
         if (string.includes(",")) {
             return stringNumber.replace(/,/g, ".");
         }
         return stringNumber;
     }
-  
-    static Splitter(string){
+
+    static Splitter(string) {
         let tempString = string.split("\r\n");
         let resultArray = [];
         for (let i = 0; i < tempString.length; i++) {
@@ -33,50 +57,29 @@ class Parser {
         }
         return resultArray;
     }
-  
-    static checkParse(array){
+
+    static checkParse(array) {
         let newArray = [];
         for (let i = 0; i < array.length; i++) {
             let numbers = parseFloat(array[i])
             if (!isNaN(numbers)) {
                 newArray.push(numbers);
-            } else{
+            } else {
                 if (array[i] === "true") {
                     let boolValue = (array[i] == "true")
                     newArray.push(boolValue);
                 }
                 else if (array[i] === "false") {
-                    let boolValue2 = (array[i] == "false")
+                    let boolValue2 = (array[i] == "true")
                     newArray.push(boolValue2);
-                } else{
+                } else {
                     newArray.push(array[i])
                 }
-                
             }
         }
         return newArray;
     }
-  }
-  
-  class EmptyStringError extends Error{
-    constructor(message){
-        super(message);
-    }
-  }
-  
-  class InvalidStringError extends Error{
-    constructor(message){
-        super(message);
-    }
-  }
-  
-  
-  class PartialInvalidStringError extends Error{
-  constructor(message, array){
-    super(message)
-    this.array = array;
-  }
-  
-  }
+}
 
-module.exports = {Parser, PartialInvalidStringError, InvalidStringError, EmptyStringError}
+
+module.exports = { Parser, PartialInvalidStringError, InvalidStringError, EmptyStringError }
